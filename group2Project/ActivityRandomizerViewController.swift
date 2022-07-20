75c33cf5a5b42296a1ba229b8541b4d6af4a5d6e
Valentina <51valentina91@gmail.com>
2022-07-20 09:37:46 -0400

    Added the randomizer and started links

--- group2Project/ActivityRandomizerViewController.swift
+++ group2Project/ActivityRandomizerViewController.swift
@@ -9,8 +9,23 @@ import UIKit
 
 class ActivityRandomizerViewController: UIViewController {
 
+    @IBOutlet weak var randomButton: UIButton!
+    @IBOutlet weak var labelBox: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+            // Do any additional setup after loading the view.
+        }
+        
+        let activites = ["Go for a walk!", "Drink some water!", "Eat a snack!", "Chat with friends/family!", "Take a warm bath!", "Write a journal entry!", "Watch your favorite show/movie!", "Listen to your favorite music!", "Do a face mask!", "Read a book!", "Meditate!", "Make art!", "Drink tea!", "start a rock collection!"]
+        
+        @IBAction func pickRandomWord(_ sender: Any) {
+            let randomAction = activites.randomElement()!
+            self.labelBox.text=randomAction;
+            
+        
+    }
 
         // Do any additional setup after loading the view.
     }
@@ -26,4 +41,3 @@ class ActivityRandomizerViewController: UIViewController {
     }
     */
 
-}
